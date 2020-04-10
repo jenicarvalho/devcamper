5aee5d505d6b7b740bef5aebefe202e89fe9a1be
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-10 17:03:00 -0300

    conexão com o banco

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import { MONGO_URI } from './vars'
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }).catch(error => console.log(error))
+
+  console.log(`MongoDB connected: ${conn.connection.host}`)
+}
+
+export default connectDB
