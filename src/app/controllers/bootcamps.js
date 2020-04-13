723455decaa267423ca83adb902bbc5b2b0bf35e
Jeniffer Carvalho <jeniffer.carvalho@cbyk.com.br>
2020-04-13 15:36:56 -0300

    adicionado model schema do bootcamp

--- src/app/controllers/bootcamps.js
+++ src/app/controllers/bootcamps.js
@@ -1,53 +1,48 @@
-/** 
+import Bootcamp from '../models/Bootcamp'
+
+/**
  * @desc    Get all Bootcamps
  * @route   GET /api/v1/bootcamps
  * @access  Public
  */
+
 export const getBootcamps = (req, res, next) => {
+  console.log(req.body)
   res.status(200).json({ info: 'all' })
 }
 
-
-/** 
+/**
  * @desc    Get single Bootcamp
- * @route   GET /api/v1/bootcamps/:id
+ * @route   GET /api/v1/b∏ootcamps/:id
  * @access  Public
  */
 export const getBootcamp = (req, res, next) => {
   res.status(200).json({ info: 'single' })
-
 }
 
-
-/** 
+/**
  * @desc    Create new Bootcamp
  * @route   POST /api/v1/bootcamps
  * @access  Private
  */
 export const createBootcamp = (req, res, next) => {
   res.status(200).json({ info: 'create' })
-
 }
 
-
-/** 
+/**
  * @desc    Update Bootcamp
  * @route   PUT /api/v1/bootcamps/:id
  * @access  Private
  */
 export const updateBootcamp = (req, res, next) => {
   res.status(200).json({ info: 'update' })
-
 }
 
-
-/** 
+/**
  * @desc    Delete Bootcamp
  * @route   DELETE /api/v1/bootcamps/:id
  * @access  Private
  */
 export const deleteBootcamp = (req, res, next) => {
   res.status(200).json({ info: 'delete' })
-
 }
-
