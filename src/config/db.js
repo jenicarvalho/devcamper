c5ddd9a039e5178e247ce193a9028d6bf48494d3
Jeniffer Carvalho <jeniffer.carvalho@cbyk.com.br>
2020-04-13 15:37:26 -0300

    quebra de linha

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose'
 import { MONGO_URI } from './vars'
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }).catch(error => console.log(error))
+  const conn = await mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .catch((error) => console.log(error))
 
   console.log(`MongoDB connected: ${conn.connection.host}`)
 }
