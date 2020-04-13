99ba9de530c019591b676fe54ca3465aa0793a29
Jeniffer Carvalho <jeniffer.carvalho@cbyk.com.br>
2020-04-13 15:37:43 -0300

    adicionado arquivo com as variaveis

---
+++ src/config/vars-example.js
@@ -0,0 +1,8 @@
+/**
+ * 1) rename this file to vars.js
+ * 2) replace the content with YOUR content
+ */
+
+export const PORT = '5555'
+export const MONGO_URI =
+  'mongodb+srv://your_user:your_pass@your-project.mongodb.net'
