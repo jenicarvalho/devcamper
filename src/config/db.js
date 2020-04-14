e40861996d3110cd98b997ca815fb8526e0aba59
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-14 18:16:26 -0300

    adicionado cor no console do mongoose

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 import { MONGO_URI } from './vars'
 
 const connectDB = async () => {
@@ -11,7 +12,7 @@ const connectDB = async () => {
     })
     .catch((error) => console.log(error))
 
-  console.log(`MongoDB connected: ${conn.connection.host}`)
+  console.log(`MongoDB connected: ${conn.connection.host}`.bgGreen.black)
 }
 
 export default connectDB
