8fe720c19a6cc73731631af6c4d0c51fbc8fe76a
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-15 00:06:09 -0300

    adicionado middleware de erros

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import bootcamps from './routes/bootcamps'
+import errorHandler from './app/middleware/error'
 
 class App {
   constructor() {
     this.server = express()
-    this.middlewares()
     this.routes()
+    this.middlewares()
   }
 
   middlewares() {
     this.server.use(cors())
     this.server.use(express.json())
+    this.server.use(errorHandler)
   }
 
   routes() {
